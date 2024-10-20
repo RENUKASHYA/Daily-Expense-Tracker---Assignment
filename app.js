c7f33e0e97a4780284b28449712b45de3ba91a16
Renu Kashyap <renukashyap616@gmail.com>
2024-10-20 14:19:08 +0530

    user routes - done

--- app.js
+++ app.js
@@ -1,4 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv').config()
+require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+const expenseRoutes = require("./routes/expenseRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
+app.use("/api/expenses", expenseRoutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+// Start server
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
